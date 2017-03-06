5e2a1ddced39fefb12445ddfba2de9e0f308fb8f
Kristina Odziomkova <Kristina.Odziomkova@anritsu.com>
2017-03-06 11:52:20 +0100

    changes to accomodate displaying the rating of 0

--- application/modules/default/library/sapp/PerformanceHelper.php
+++ application/modules/default/library/sapp/PerformanceHelper.php
@@ -1253,6 +1253,10 @@ public static function manager_questions_privileges($questionarray,$appraisalid,
 			   	
 			   	 ?>
 			   	 <div id="ratingsdiv" class="ratings_div" style="display: none;">
+                
+                 <div class="ratings_block">
+                    <span><?php echo "0 - Not Rated" ?> </span>
+                 </div>
 	   <?php 	
 			   	 for($i=0;$i<$ratingsstar;$i++)
 			   	 {
